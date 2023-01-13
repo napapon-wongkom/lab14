5a1e59f0081b6e8201a4de376c6fd899d691bebc
napapon-wongkom <dot.wongkom@gmail.com>
2023-01-13 17:42:22 +0700

    lab14_2.cpp

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,32 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void updateImage(bool IMG[][M],int s,int x,int y){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+            double brush = s-1,paintcoor = sqrt(pow(i-x,2)+pow(j-y,2));
+            if(paintcoor<=brush){
+                IMG[i][j] = 1;
+            }
+        }
+    }
+    
+}
+void showImage(const bool IMG[][M]){
+    cout<<"------------------------------------------------------------------------\n";
+    for(int i =0;i<N;i++){
+        cout<<"|";
+        for(int j =0;j<M;j++){
+            
+            if(IMG[i][j]==1){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            }
+            
+        }
+        cout<<"|";
+        cout<<"\n";
+    }
+    cout<<"------------------------------------------------------------------------\n";
+}

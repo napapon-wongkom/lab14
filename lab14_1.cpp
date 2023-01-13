41d8374cfa735b32beca9d3ac20e3f003cf7e576
napapon-wongkom <dot.wongkom@gmail.com>
2023-01-13 16:41:43 +0700

    lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,39 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+    double sum = 0,sumz =0,max = A[0],min = A[N-1],sumofmulti=A[0],sumofdivide=0;
+    for(int i =0;i<N;i++){
+        sum += A[i];
+    }
+    double mean = sum/N;
+    for(int i =0;i<N;i++){
+        sumz += pow(A[i]-mean,2);
+    }
+    double SD = sqrt(sumz/N);
+    for(int i=1;i<N;i++){
+        sumofmulti *= A[i];
+    }
+    double GM = pow(sumofmulti,1.0/N);
+    for(int i=0;i<N;i++){
+        sumofdivide += (1.0/A[i]);
+    }
+    double HM = N/sumofdivide;
+    for(int i=1;i<N;i++){
+        if(max<A[i]){
+            max = A[i];
+        }
+    }
+    for(int i=1;i<N;i++){
+        if(min>A[i]){
+            min = A[i];
+        }
+    }
+    
+    B[0] = mean;
+    B[1] = SD;
+    B[2] = GM;
+    B[3] = HM;
+    B[4] = max;
+    B[5] = min;
+}
